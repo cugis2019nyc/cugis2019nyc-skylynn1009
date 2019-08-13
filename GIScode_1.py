dd04cee35d6738a3827c24db2149db22908f954e
skylynn1009 <54040214+skylynn1009@users.noreply.github.com>
2019-08-13 13:43:23 -0400

    Add files via upload

---
+++ GIScode_1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world") #greeting
+print("my name is sky")
+print("101")
+print("5*2")
+5/2
+5*2
+5+2
+5-2
+5^2
+5**2
+8/9*3
+
+def add(a,b):
+    add = a+b
+    print(add) 
+
+add(123,789)    
+def add(a,b,c):
+    add = a+b+c
+    print(add)
+    
+add
+    
+add(967,23,56)    
+
+print("the sum of 967,23 and 56 is 1046")
+
+def area(a,b):
+    area = a*b/2
+    print("the area of a triangle with base",a, "and height",b,"is", area)
+    
+    area(8,7)
+area(6,9)    
+    
+    
